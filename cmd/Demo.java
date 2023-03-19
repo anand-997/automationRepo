f99af77b347f5902e9d66b9a92e38e8a08fdce94
anand-997 <anand20.sp@gmail.com>
2023-03-19 15:35:25 +0530

    my second commit

---
+++ cmd/Demo.java
@@ -0,0 +1,15 @@
+public class Demo {
+ public static void main (String[] args) {
+int a = 50, b = 10 ;
+
+if (a>b)
+{
+	System.out.println("a is greater");
+}
+else
+{
+	System.out.println("b is greater");
+}
+
+}
+}
